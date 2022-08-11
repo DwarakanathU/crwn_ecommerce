bea99c5197c06f82851b2ea6c9cf69b21cbd1b24
DwarakanathU <dwarakanathummadisetti@gmail.com>
2022-08-11 10:21:56 +0530

    added sass to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import "./categories.styles.scss";
+
 const App = () => {
-  return <div>Crown Ecommerce</div>;
+  const catergories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Men",
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {catergories.map((category) => (
+        <div className="category-container">
+          <div className="background-image"></div>
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
